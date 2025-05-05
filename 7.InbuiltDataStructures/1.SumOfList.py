b17c6d2ad2a21e18e5eacabf924bceace56263d3
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2025-05-05 20:57:07 +0530

    List Operations
    Sum of List

---
+++ 7.InbuiltDataStructures/1.SumOfList.py
@@ -0,0 +1,12 @@
+def sumOfList(numbers):
+    result = 0
+    for i in numbers:
+        result += i
+    return result
+number = [1,2,3,4,5,6]
+r1 = sumOfList(number)
+print(r1)
+
+number = [11,21,33,44,53,26]
+r2 = sumOfList(number)
+print(r2)
